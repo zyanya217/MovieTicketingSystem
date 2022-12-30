e2ec77afd90a4af00dacd9d3be8870c197f3ef4b
zyanya217 <a0903203219@gmail.com>
2022-12-30 11:23:35 +0800

    1230

--- dbo.Table.sql
+++ dbo.Table.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[Table]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Event] VARCHAR(50) NOT NULL, 
-    [Place] NCHAR(10) NULL, 
-    [Date] DATE NOT NULL, 
-    [Time] DATETIME NULL
+	[訂單編號] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [日期] TIME NOT NULL, 
+    [電影名稱] NCHAR(10) NOT NULL, 
+    [票種] NCHAR(10) NOT NULL, 
+    [座位] NCHAR(5) NOT NULL, 
+    [影廳] NCHAR(10) NULL, 
+    [場次] NCHAR(10) NULL, 
+    [距離觀影日還有幾天] NCHAR(10) NULL
 )
