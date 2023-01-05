42d05f4c6633556f816578be9b96f7b2bfc3216d
zyanya217 <a0903203219@gmail.com>
2023-01-05 23:25:09 +0800

    0105修改金額與注意框

--- BookingForm.vb
+++ BookingForm.vb
@@ -1,4 +1,11 @@
 ﻿Public Class BookingForm
+
+    '設票券變數
+    Private discount_ticket As Double
+    Private regular As Double
+    Private group_ticket As Double
+    Private Total As Double
+
     Private Sub BookingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Label1.BackColor = Color.Transparent
         Label2.BackColor = Color.Transparent
@@ -18,7 +25,6 @@
         Dim frm As New DetailsForm
         frm.SourceForm = Me
         'frm.ShowDialog() '未測試該功能
-        ' LblTotal
 
     End Sub
     Private Sub BtnMenu_Click(sender As Object, e As EventArgs) Handles BtnMenu.Click
@@ -30,7 +36,7 @@
     Private Sub BtnNext_Click(sender As Object, e As EventArgs) Handles BtnNext.Click
 
         '判斷欄位是否有空值，若無空值，則帶入值
-        If TextBox1.Text IsNot Nothing And (Len(TextBox1.Text) = 10) Then
+        If (TextBox1.Text IsNot Nothing) And (Len(TextBox1.Text) = 10) Then
 
 
             '跳轉化面至下一頁
@@ -39,7 +45,7 @@
         Else
             '判斷是否有輸入是否合理
             Dim Input As DialogResult
-            Input = MessageBox.Show("有欄位還沒被填寫或電話輸入不合理。A field has not been filled or the phone input is invalid.", "注意!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Input = MessageBox.Show("有欄位還沒被填寫或電話輸入不合理。" + vbCrLf + “A field has not been filled or the phone input is invalid.", "注意!", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
 
 
@@ -48,9 +54,28 @@
 
     Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
 
+        Double.TryParse(ListBox2.Text, discount_ticket)
+        Double.TryParse(ListBox3.Text, regular)
+        Double.TryParse(ListBox4.Text, group_ticket)
+        Total = discount_ticket * 100 + regular * 200 + group_ticket * 150
+        LblTotal.Text = Total.ToString
     End Sub
 
-    Private Sub LblTotal_Click(sender As Object, e As EventArgs) Handles LblTotal.Click
+    Private Sub ListBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox3.SelectedIndexChanged
+
+        Double.TryParse(ListBox2.Text, discount_ticket)
+        Double.TryParse(ListBox3.Text, regular)
+        Double.TryParse(ListBox4.Text, group_ticket)
+        Total = discount_ticket * 100 + regular * 200 + group_ticket * 150
+        LblTotal.Text = Total.ToString
+    End Sub
+
+    Private Sub ListBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox4.SelectedIndexChanged
+        Double.TryParse(ListBox2.Text, discount_ticket)
+        Double.TryParse(ListBox3.Text, regular)
+        Double.TryParse(ListBox4.Text, group_ticket)
+        Total = discount_ticket * 100 + regular * 200 + group_ticket * 150
+        LblTotal.Text = Total.ToString
 
     End Sub
 End Class
