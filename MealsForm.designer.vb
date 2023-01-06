adf2bfb2a499ba4818d6b1b12c0adea9fe2a1a81
jameschiu1023 <jameschiu1023@gmail.com>
2023-01-06 11:52:44 +0800

    1/6

--- MealsForm.designer.vb
+++ MealsForm.designer.vb
@@ -34,18 +34,18 @@ Partial Class MealsForm
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.ListBox3 = New System.Windows.Forms.ListBox()
-        Me.ListBox4 = New System.Windows.Forms.ListBox()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.ListBox5 = New System.Windows.Forms.ListBox()
-        Me.ListBox6 = New System.Windows.Forms.ListBox()
-        Me.ListBox7 = New System.Windows.Forms.ListBox()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label11 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox7 = New System.Windows.Forms.ComboBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -116,7 +116,7 @@ Partial Class MealsForm
         Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label1.Location = New System.Drawing.Point(101, 267)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(156, 55)
+        Me.Label1.Size = New System.Drawing.Size(102, 36)
         Me.Label1.TabIndex = 25
         Me.Label1.Text = "爆米花"
         '
@@ -127,7 +127,7 @@ Partial Class MealsForm
         Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label2.Location = New System.Drawing.Point(367, 267)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(156, 55)
+        Me.Label2.Size = New System.Drawing.Size(102, 36)
         Me.Label2.TabIndex = 26
         Me.Label2.Text = "大亨堡"
         '
@@ -138,7 +138,7 @@ Partial Class MealsForm
         Me.Label3.Font = New System.Drawing.Font("微軟正黑體", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label3.Location = New System.Drawing.Point(635, 267)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(112, 55)
+        Me.Label3.Size = New System.Drawing.Size(73, 36)
         Me.Label3.TabIndex = 27
         Me.Label3.Text = "可樂"
         '
@@ -149,7 +149,7 @@ Partial Class MealsForm
         Me.Label4.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label4.Location = New System.Drawing.Point(36, 326)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(143, 45)
+        Me.Label4.Size = New System.Drawing.Size(95, 30)
         Me.Label4.TabIndex = 28
         Me.Label4.Text = "大(60$)"
         '
@@ -160,7 +160,7 @@ Partial Class MealsForm
         Me.Label5.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label5.Location = New System.Drawing.Point(36, 375)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(143, 45)
+        Me.Label5.Size = New System.Drawing.Size(95, 30)
         Me.Label5.TabIndex = 29
         Me.Label5.Text = "中(50$)"
         '
@@ -171,49 +171,9 @@ Partial Class MealsForm
         Me.Label6.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label6.Location = New System.Drawing.Point(36, 421)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(143, 45)
+        Me.Label6.Size = New System.Drawing.Size(95, 30)
         Me.Label6.TabIndex = 30
         Me.Label6.Text = "小(40$)"
-        '
-        'ListBox1
-        '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 40
-        Me.ListBox1.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox1.Location = New System.Drawing.Point(138, 326)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox1.TabIndex = 31
-        '
-        'ListBox2
-        '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.ItemHeight = 40
-        Me.ListBox2.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox2.Location = New System.Drawing.Point(138, 375)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox2.TabIndex = 32
-        '
-        'ListBox3
-        '
-        Me.ListBox3.FormattingEnabled = True
-        Me.ListBox3.ItemHeight = 40
-        Me.ListBox3.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox3.Location = New System.Drawing.Point(138, 421)
-        Me.ListBox3.Name = "ListBox3"
-        Me.ListBox3.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox3.TabIndex = 33
-        '
-        'ListBox4
-        '
-        Me.ListBox4.FormattingEnabled = True
-        Me.ListBox4.ItemHeight = 40
-        Me.ListBox4.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox4.Location = New System.Drawing.Point(382, 326)
-        Me.ListBox4.Name = "ListBox4"
-        Me.ListBox4.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox4.TabIndex = 35
         '
         'Label7
         '
@@ -222,39 +182,9 @@ Partial Class MealsForm
         Me.Label7.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label7.Location = New System.Drawing.Point(321, 326)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(83, 45)
+        Me.Label7.Size = New System.Drawing.Size(55, 30)
         Me.Label7.TabIndex = 34
         Me.Label7.Text = "50$"
-        '
-        'ListBox5
-        '
-        Me.ListBox5.FormattingEnabled = True
-        Me.ListBox5.ItemHeight = 40
-        Me.ListBox5.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox5.Location = New System.Drawing.Point(641, 421)
-        Me.ListBox5.Name = "ListBox5"
-        Me.ListBox5.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox5.TabIndex = 41
-        '
-        'ListBox6
-        '
-        Me.ListBox6.FormattingEnabled = True
-        Me.ListBox6.ItemHeight = 40
-        Me.ListBox6.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox6.Location = New System.Drawing.Point(641, 375)
-        Me.ListBox6.Name = "ListBox6"
-        Me.ListBox6.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox6.TabIndex = 40
-        '
-        'ListBox7
-        '
-        Me.ListBox7.FormattingEnabled = True
-        Me.ListBox7.ItemHeight = 40
-        Me.ListBox7.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
-        Me.ListBox7.Location = New System.Drawing.Point(641, 326)
-        Me.ListBox7.Name = "ListBox7"
-        Me.ListBox7.Size = New System.Drawing.Size(110, 4)
-        Me.ListBox7.TabIndex = 39
         '
         'Label8
         '
@@ -263,7 +193,7 @@ Partial Class MealsForm
         Me.Label8.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label8.Location = New System.Drawing.Point(539, 421)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(143, 45)
+        Me.Label8.Size = New System.Drawing.Size(95, 30)
         Me.Label8.TabIndex = 38
         Me.Label8.Text = "小(10$)"
         '
@@ -274,7 +204,7 @@ Partial Class MealsForm
         Me.Label9.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label9.Location = New System.Drawing.Point(539, 375)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(143, 45)
+        Me.Label9.Size = New System.Drawing.Size(95, 30)
         Me.Label9.TabIndex = 37
         Me.Label9.Text = "中(20$)"
         '
@@ -285,7 +215,7 @@ Partial Class MealsForm
         Me.Label10.Font = New System.Drawing.Font("微軟正黑體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label10.Location = New System.Drawing.Point(539, 326)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(143, 45)
+        Me.Label10.Size = New System.Drawing.Size(95, 30)
         Me.Label10.TabIndex = 36
         Me.Label10.Text = "大(30$)"
         '
@@ -296,28 +226,91 @@ Partial Class MealsForm
         Me.Label11.Font = New System.Drawing.Font("微軟正黑體", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
         Me.Label11.Location = New System.Drawing.Point(25, 29)
         Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(291, 81)
+        Me.Label11.Size = New System.Drawing.Size(196, 55)
         Me.Label11.TabIndex = 42
         Me.Label11.Text = "選擇餐點"
         '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox1.Location = New System.Drawing.Point(125, 326)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox1.TabIndex = 43
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox2.Location = New System.Drawing.Point(125, 371)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox2.TabIndex = 44
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox3.Location = New System.Drawing.Point(125, 421)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox3.TabIndex = 45
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox4.Location = New System.Drawing.Point(373, 326)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox4.TabIndex = 46
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox5.Location = New System.Drawing.Point(629, 326)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox5.TabIndex = 47
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox6.Location = New System.Drawing.Point(629, 375)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox6.TabIndex = 48
+        '
+        'ComboBox7
+        '
+        Me.ComboBox7.FormattingEnabled = True
+        Me.ComboBox7.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox7.Location = New System.Drawing.Point(629, 421)
+        Me.ComboBox7.Name = "ComboBox7"
+        Me.ComboBox7.Size = New System.Drawing.Size(121, 34)
+        Me.ComboBox7.TabIndex = 49
+        '
         'MealsForm
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(20.0!, 40.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 26.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(914, 540)
+        Me.Controls.Add(Me.ComboBox7)
+        Me.Controls.Add(Me.ComboBox6)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label11)
-        Me.Controls.Add(Me.ListBox5)
-        Me.Controls.Add(Me.ListBox6)
-        Me.Controls.Add(Me.ListBox7)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label10)
-        Me.Controls.Add(Me.ListBox4)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.ListBox3)
-        Me.Controls.Add(Me.ListBox2)
-        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
@@ -353,16 +346,16 @@ Partial Class MealsForm
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents ListBox2 As ListBox
-    Friend WithEvents ListBox3 As ListBox
-    Friend WithEvents ListBox4 As ListBox
     Friend WithEvents Label7 As Label
-    Friend WithEvents ListBox5 As ListBox
-    Friend WithEvents ListBox6 As ListBox
-    Friend WithEvents ListBox7 As ListBox
     Friend WithEvents Label8 As Label
     Friend WithEvents Label9 As Label
     Friend WithEvents Label10 As Label
     Friend WithEvents Label11 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox6 As ComboBox
+    Friend WithEvents ComboBox7 As ComboBox
 End Class
