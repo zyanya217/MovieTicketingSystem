0e494685c8f31daf7dd45d205cdea90e54cbab9d
jameschiu1023 <jameschiu1023@gmail.com>
2023-01-10 12:58:05 +0800

    1/8

--- MealsForm.vb
+++ MealsForm.vb
@@ -5,8 +5,8 @@
 
     Private Sub BtnNext_Click(sender As Object, e As EventArgs) Handles BtnNext.Click
         Dim price As Int16
-        price = ComboBox1.ToString * 60 + ComboBox2.ToString * 50 + ComboBox3.ToString * 40 +
-            ComboBox4.ToString * 50 + ComboBox5.ToString * 30 + ComboBox6.ToString * 20 + ComboBox7.ToString * 10
+        price = ComboBox1.SelectedIndex.ToString * 60 + ComboBox2.SelectedIndex.ToString * 50 + ComboBox3.SelectedIndex.ToString * 40 +
+            ComboBox4.SelectedIndex.ToString * 50 + ComboBox5.SelectedIndex.ToString * 30 + ComboBox6.SelectedIndex.ToString * 20 + ComboBox7.SelectedIndex.ToString * 10
         Dim sForm As New SeatForm
         sForm.Totalseat = Totalseat
         sForm.Mealsprice = price
