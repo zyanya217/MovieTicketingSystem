b4b35d4ed9cffda806985da50e131479c4a6f961
jameschiu1023 <jameschiu1023@gmail.com>
2023-01-12 21:30:31 +0800

    1/11

--- 3.Booking/MealsForm.vb
+++ 3.Booking/MealsForm.vb
@@ -76,7 +76,195 @@ Public Class MealsForm
         Me.Hide()
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist + vbCrLf + price
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist + vbCrLf + price
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox4.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist.ToString + vbCrLf + price.ToString
+    End Sub
+
+    Private Sub ComboBox5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox5.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist.ToString + vbCrLf + price.ToString
+    End Sub
+
+    Private Sub ComboBox6_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox6.SelectedIndexChanged
         mealslist = ""
         price = 0
         If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
