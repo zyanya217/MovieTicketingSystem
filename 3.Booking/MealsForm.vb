db9e93226ee9be15f7ffc7b1689f46448f66ce90
jameschiu1023 <jameschiu1023@gmail.com>
2023-01-12 21:32:20 +0800

    1/11

--- 3.Booking/MealsForm.vb
+++ 3.Booking/MealsForm.vb
@@ -119,7 +119,7 @@ Public Class MealsForm
                 price += ComboBox7.SelectedIndex * 10
             End If
         End If
-        Label12.Text = mealslist + vbCrLf + price
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
     End Sub
 
     Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
@@ -165,10 +165,53 @@ Public Class MealsForm
                 price += ComboBox7.SelectedIndex * 10
             End If
         End If
-        Label12.Text = mealslist + vbCrLf + price
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
     End Sub
 
     Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
 
     End Sub
 
@@ -215,7 +258,8 @@ Public Class MealsForm
                 price += ComboBox7.SelectedIndex * 10
             End If
         End If
-        Label12.Text = mealslist.ToString + vbCrLf + price.ToString
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
+
     End Sub
 
     Private Sub ComboBox5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox5.SelectedIndexChanged
@@ -261,7 +305,8 @@ Public Class MealsForm
                 price += ComboBox7.SelectedIndex * 10
             End If
         End If
-        Label12.Text = mealslist.ToString + vbCrLf + price.ToString
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
+
     End Sub
 
     Private Sub ComboBox6_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox6.SelectedIndexChanged
@@ -307,7 +352,58 @@ Public Class MealsForm
                 price += ComboBox7.SelectedIndex * 10
             End If
         End If
-        Button1.Text = mealslist.ToString + vbCrLf + "共:" + price.ToString
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
 
+    End Sub
+
+    Private Sub ComboBox7_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox7.SelectedIndexChanged
+        mealslist = ""
+        price = 0
+        If ComboBox1.SelectedIndex = -1 AndAlso ComboBox2.SelectedIndex = -1 AndAlso ComboBox3.SelectedIndex = -1 Then
+            mealslist = ""
+        Else
+            mealslist += "爆米花"
+            If ComboBox1.SelectedIndex <> -1 Then
+                mealslist += " (大)x" + ComboBox1.SelectedIndex.ToString
+                price += ComboBox1.SelectedIndex * 60
+            End If
+            If ComboBox2.SelectedIndex <> -1 Then
+                mealslist += " (中)x" + ComboBox2.SelectedIndex.ToString
+                price += ComboBox2.SelectedIndex * 50
+            End If
+            If ComboBox3.SelectedIndex <> -1 Then
+                mealslist += " (小)x" + ComboBox3.SelectedIndex.ToString
+                price += ComboBox3.SelectedIndex * 30
+            End If
+        End If
+        If ComboBox4.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "大亨堡" + "x" + ComboBox4.SelectedIndex.ToString
+            price += ComboBox4.SelectedIndex * 50
+        End If
+        If ComboBox5.SelectedIndex = -1 AndAlso ComboBox6.SelectedIndex = -1 AndAlso ComboBox7.SelectedIndex = -1 Then
+            mealslist += ""
+        Else
+            mealslist += vbCrLf + "可樂"
+            If ComboBox5.SelectedIndex <> -1 Then
+                mealslist += "(大)x" + ComboBox5.SelectedIndex.ToString
+                price += ComboBox5.SelectedIndex * 30
+            End If
+            If ComboBox6.SelectedIndex <> -1 Then
+                mealslist += "(中)x" + ComboBox6.SelectedIndex.ToString
+                price += ComboBox6.SelectedIndex * 20
+            End If
+            If ComboBox7.SelectedIndex <> -1 Then
+                mealslist += "(小)x" + ComboBox7.SelectedIndex.ToString
+                price += ComboBox7.SelectedIndex * 10
+            End If
+        End If
+        Label12.Text = mealslist.ToString + vbCrLf + "共" + price.ToString + "元"
+
+    End Sub
+
+    Private Sub MealsForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Label12.BackColor = Color.Transparent
     End Sub
 End Class
